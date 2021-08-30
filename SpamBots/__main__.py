4ccbcb7d43b8da5810c8fe738fc4984e0cc9aa82
himzzGit <89766348+himzzGit@users.noreply.github.com>
2021-08-30 22:28:19 +0530

    Update __main__.py

--- SpamBots/__main__.py
+++ SpamBots/__main__.py
@@ -2,7 +2,7 @@ import glob
 from pathlib import Path
 from SpamBots.utils import load_plugins
 import logging
-from . import UstaD, UstaD2, UstaD3, UstaD5 , UstaD6, UstaD7, UstaD8, UstaD9, UstaD10
+from . import Legend, Legend2, Legend3, Legend4, Legend5, Legend6, Legend7, Legend8, Legend9, Legend10
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
@@ -16,35 +16,36 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
 
 print("Successfully deployed!")
-print("Enjoy! Do visit @USTAD_SPAM_BOTS")
+print("Original Repo of: @OFFICIAL_SAMEER")
+print("Also Join @LegendBot_Support")
 
 if __name__ == "__main__":
-    UstaD.run_until_disconnected()
+    Legend.run_until_disconnected()
     
 if __name__ == "__main__":
-    UstaD2.run_until_disconnected()
+    Legend2.run_until_disconnected()
 
 if __name__ == "__main__":
-    UstaD3.run_until_disconnected()
+    Legend3.run_until_disconnected()
     
 if __name__ == "__main__":
-    UstaD4.run_until_disconnected()
+    Legend4.run_until_disconnected()
 
 if __name__ == "__main__":
-    UstaD5.run_until_disconnected()
+    Legend5.run_until_disconnected()
     
 if __name__ == "__main__":
-    UstaD6.run_until_disconnected()
+    Legend6.run_until_disconnected()
 
 if __name__ == "__main__":
-    UstaD7.run_until_disconnected()
+    Legend7.run_until_disconnected()
     
 if __name__ == "__main__":
-    UstaD8.run_until_disconnected()
+    Legend8.run_until_disconnected()
 
 if __name__ == "__main__":
-    UstaD9.run_until_disconnected()
+    Legend9.run_until_disconnected()
     
 if __name__ == "__main__":
-    UstaD10.run_until_disconnected()
+    Legend10.run_until_disconnected()
 
