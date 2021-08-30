c0dd7d8a94f40d761e6f43e9e53b04d2874b87a0
himzzGit <89766348+himzzGit@users.noreply.github.com>
2021-08-30 22:36:57 +0530

    Update bigspam.py

--- SpamBots/plugins/bigspam.py
+++ SpamBots/plugins/bigspam.py
@@ -19,7 +19,7 @@ import os
 from telethon import events
 from telethon import functions, types
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
-from .. import UstaD, UstaD2, UstaD3, UstaD4, UstaD5, UstaD6, UstaD7, UstaD8, UstaD9, UstaD10, SUDO_USERS
+from .. import Legend, Legend2, Legend3, Legend4, Legend5, Legend6, Legend7, Legend8, Legend9, Legend10, SUDO_USERS
 
 SMEX_USERS = []
 for x in SUDO_USERS:
@@ -27,24 +27,24 @@ for x in SUDO_USERS:
 
 
 
-@UstaD.on(events.NewMessage(pattern=".bigspam"))
-@UstaD2.on(events.NewMessage(pattern=".bigspam"))
-@UstaD3.on(events.NewMessage(pattern=".bigspam"))
-@UstaD4.on(events.NewMessage(pattern=".bigspam"))
-@UstaD5.on(events.NewMessage(pattern=".bigspam"))
-@UstaD6.on(events.NewMessage(pattern=".bigspam"))
-@UstaD7.on(events.NewMessage(pattern=".bigspam"))
-@UstaD8.on(events.NewMessage(pattern=".bigspam"))
-@UstaD9.on(events.NewMessage(pattern=".bigspam"))
-@UstaD10.on(events.NewMessage(pattern=".bigspam"))
+@Legend.on(events.NewMessage(pattern=".bigspam"))
+@Legend2.on(events.NewMessage(pattern=".bigspam"))
+@Legend3.on(events.NewMessage(pattern=".bigspam"))
+@Legend4.on(events.NewMessage(pattern=".bigspam"))
+@Legend5.on(events.NewMessage(pattern=".bigspam"))
+@Legend6.on(events.NewMessage(pattern=".bigspam"))
+@Legend7.on(events.NewMessage(pattern=".bigspam"))
+@Legend8.on(events.NewMessage(pattern=".bigspam"))
+@Legend9.on(events.NewMessage(pattern=".bigspam"))
+@Legend10.on(events.NewMessage(pattern=".bigspam"))
 async def spam(e):
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
-        Ustad = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        Legend = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         smex = await e.get_reply_message()
-        if len(Ustad) == 2:
-            message = str(Ustad[1])
-            counter = int(Ustad[0])
+        if len(Legend) == 2:
+            message = str(Legend[1])
+            counter = int(Legend[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "typing"):
                     if e.reply_to_msg_id:
@@ -53,7 +53,7 @@ async def spam(e):
                         await e.client.send_message(e.chat_id, message)
                     await asyncio.sleep(0.1)
         elif e.reply_to_msg_id and smex.media:
-            counter = int(Ustad[0])
+            counter = int(Legend[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "document"):
                     smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
@@ -61,7 +61,7 @@ async def spam(e):
                 await asyncio.sleep(0.1)
         elif e.reply_to_msg_id and smex.text:
             message = smex.text
-            counter = int(Ustad[0])
+            counter = int(Legend[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "typing"):
                     await e.client.send_message(e.chat_id, message)
